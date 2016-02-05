fbf58a408d9c0427b0b6093c2e7220d5806de928
Duke Miller <Duke Miller>
2016-02-05 00:16:39 -0700

    Rename toolbox icons; Add functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,10 @@ namespace anime_downloader {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void button_folder_Click(object sender, RoutedEventArgs e) {
+            Process.Start(@"D:\Output\anime downloader");
         }
     }
 }
