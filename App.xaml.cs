2ed2ec29b6ae1193097b1b0f3cf2aad96b0e81a9
Duke Miller <Duke Miller>
2016-02-05 00:03:01 -0700

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace anime_downloader {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
