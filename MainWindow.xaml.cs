237eb07434ab6e69b3aedef9db63dec57a1da0af
Duke Miller <Duke Miller>
2016-02-05 00:34:01 -0700

    Add auto generating playlist function

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,16 +16,38 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace anime_downloader {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        String animeFolder;
+
         public MainWindow() {
             InitializeComponent();
+
+            animeFolder = @"D:\Output\anime downloader";
         }
 
         private void button_folder_Click(object sender, RoutedEventArgs e) {
-            Process.Start(@"D:\Output\anime downloader");
+            Process.Start(animeFolder);
+        }
+
+        private void button_playlist_Click(object sender, RoutedEventArgs e) {
+            string[] folders = Directory.GetDirectories(animeFolder)
+                .Where(s => !s.EndsWith("torrents") && !s.EndsWith("Grace") && !s.EndsWith("Watched"))
+                .ToArray();
+
+            using (StreamWriter file = new StreamWriter(path: animeFolder + @"\playlist.m3u", 
+                                                        append: false)) {
+                foreach(String folder in folders)
+                    file.WriteLine(String.Join("\n", Directory.GetFiles(folder)));
+            }
+
+            // MessageBox.Show(String.Join(", ", folders));
+
+            // System.Windows.MessageBox.Show(String.Join(", ", ));
+            // Directory.GetDirectories(animeFolder);
         }
     }
 }
