6646cdc123fd2ca11b05b7e87c1e739793b982c2
Duke <dukemiller928@hotmail.com>
2016-09-16 01:25:22 -0700

    Fix to file finding distance algorithm for more accuracy
    The changes are based upon what actually gives correct values assessed by human judgement, so im not too afraid to say here that i'm entirely entirely arbitrary for how this actually calculates values

--- anime-downloader/Classes/Distances/GroupFileDistance.cs
+++ anime-downloader/Classes/Distances/GroupFileDistance.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using anime_downloader.Classes.File;
 
@@ -5,11 +6,11 @@ namespace anime_downloader.Classes.Distances
 {
     public class GroupFileDistance
     {
-        private static readonly string[] CommonTokens = { "no", "na" };
+        private static readonly string[] CommonTokens = { "no", "to", "na" };
 
-        private const double Bias = 3.25;
+        private const double Bias = 2.35;
 
-        private readonly double _relevance;
+        private readonly double _ratioToOriginal;
 
         private readonly double _distance;
 
@@ -19,23 +20,45 @@ namespace anime_downloader.Classes.Distances
         /// </remarks>
         private readonly int _singleWordPenalty;
 
-        public double Distance => _distance * (Bias + _singleWordPenalty - _relevance);
+        /// <remarks>
+        ///     Such was the case that words with no matches would match more closely to
+        ///     titles than ones with even half of the phrase matched, so if the word has
+        ///     no matches then incur a harsh penalty
+        /// </remarks>
+        private readonly int _noMatchPenalty;
+
+        private readonly int _allMatchBonus;
+
+        public double Distance => (_singleWordPenalty * 20) + _distance * (Bias + _noMatchPenalty - _ratioToOriginal - _allMatchBonus);
 
         public IGrouping<string, AnimeFile> Group { get; }
+
+        private static string[] Scrub(string str)
+        {
+            return str.ToLower().Replace(":", "").Replace("-", "").OnlyLettersAndSpace().Split(' ').Distinct().Except(CommonTokens).ToArray();
+        }
 
         public GroupFileDistance(IGrouping<string, AnimeFile> grouping, Anime anime)
         {
             Group = grouping;
-            var namesplit = anime.Name.ToLower().OnlyLettersAndSpace().Split(' ').Distinct().Except(CommonTokens).ToArray();
-            var groupsplit = grouping.Key.ToLower().OnlyLettersAndSpace().Split(' ').Distinct().Except(CommonTokens).ToArray();
-            _distance = Methods.LevenshteinDistance(grouping.Key, anime.Name);
-            _relevance = (double) groupsplit.Count(a => namesplit.Contains(a)) / groupsplit.Length;
-            _singleWordPenalty = namesplit.Length == 1 && groupsplit.Length == 1 ? 1 : 0;
+
+            var namesplit = Scrub(anime.Name);
+            var groupsplit = Scrub(grouping.Key);
+
+            _distance = Methods.LevenshteinDistance(anime.Name, grouping.Key);
+            _ratioToOriginal = (double) groupsplit.Count(a => namesplit.Contains(a)) / groupsplit.Length;
+            _singleWordPenalty = namesplit.Length == 1 && _ratioToOriginal <= 0 ? 1 : 0;
+            _noMatchPenalty = namesplit.Length > 1 && _ratioToOriginal <= 0 ? 8 : 0;
+            _allMatchBonus = namesplit.Count(a => groupsplit.Contains(a)) == namesplit.Length ? 1 : 0;
+
+            // Console.WriteLine($@"{_ratioToOriginal} {_distance} {Distance} {grouping.Key}");
+            
             /*
             Console.WriteLine($"'{anime.Name}' COMPARED TO '{grouping.Key}'");
-            Console.WriteLine($"-- {_distance}, {_relevance}, {Distance}");
+            Console.WriteLine($"-- {_distance}, {_ratioToOriginal}, {Distance}");
             Console.WriteLine($"-- {namesplit.CommaJoined()} || {groupsplit.CommaJoined()}");
             */
+            
         }
     }
 }
