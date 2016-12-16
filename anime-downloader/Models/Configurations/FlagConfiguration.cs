1bbb2d5be5dfaee9784a548b343b483859f4af98
Duke <dukemiller928@hotmail.com>
2016-12-16 15:57:20 -0700

    Move configurations to new namespace, add observable properties

---
+++ anime-downloader/Models/Configurations/FlagConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Xml.Serialization;
+using GalaSoft.MvvmLight;
+
+namespace anime_downloader.Models.Configurations
+{
+    [Serializable]
+    public class FlagConfiguration: ObservableObject
+    {
+        private bool _sortByReversed;
+        private bool _exitOnClose;
+        private bool _alwaysShowTray;
+        private bool _individualShowFolders;
+        private bool _onlyWhitelisted;
+
+        [XmlAttribute("sort_by_reversed")]
+        public bool SortByReversed
+        {
+            get { return _sortByReversed; }
+            set { Set(() => SortByReversed, ref _sortByReversed, value); }
+        }
+
+        [XmlAttribute("exit_on_close")]
+        public bool ExitOnClose
+        {
+            get { return _exitOnClose; }
+            set { Set(() => ExitOnClose, ref _exitOnClose, value); }
+        }
+
+        [XmlAttribute("always_show_tray")]
+        public bool AlwaysShowTray
+        {
+            get { return _alwaysShowTray; }
+            set { Set(() => AlwaysShowTray, ref _alwaysShowTray, value); }
+        }
+
+        [XmlAttribute("show_folders")]
+        public bool IndividualShowFolders
+        {
+            get { return _individualShowFolders; }
+            set { Set(() => IndividualShowFolders, ref _individualShowFolders, value); }
+        }
+
+        [XmlAttribute("only_whitelisted")]
+        public bool OnlyWhitelisted
+        {
+            get { return _onlyWhitelisted; }
+            set { Set(() => OnlyWhitelisted, ref _onlyWhitelisted, value); }
+        }
+    }
+}
---
+++ anime-downloader/Models/Configurations/PathConfiguration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using GalaSoft.MvvmLight;
+
+namespace anime_downloader.Models.Configurations
+{
+    [Serializable]
+    public class PathConfiguration: ObservableObject
+    {
+        private string _unwatched;
+        private string _watched;
+        private string _torrents;
+        private string _torrentDownloader;
+        private string _playlist;
+        private string _logging;
+
+        [XmlAttribute("unwatched")]
+        public string Unwatched
+        {
+            get { return _unwatched; }
+            set { Set(() => Unwatched, ref _unwatched, value); }
+        }
+
+        [XmlAttribute("watched")]
+        public string Watched
+        {
+            get { return _watched; }
+            set { Set(() => Watched, ref _watched, value); }
+        }
+
+        [XmlAttribute("torrents")]
+        public string Torrents
+        {
+            get { return _torrents; }
+            set { Set(() => Torrents, ref _torrents, value); }
+        }
+
+        [XmlAttribute("torrent_downloader")]
+        public string TorrentDownloader
+        {
+            get { return _torrentDownloader; }
+            set { Set(() => TorrentDownloader, ref _torrentDownloader, value); }
+        }
+
+        [XmlAttribute("playlist")]
+        public string Playlist
+        {
+            get { return _playlist; }
+            set { Set(() => Playlist, ref _playlist, value); }
+        }
+
+        [XmlAttribute("log")]
+        public string Logging
+        {
+            get { return _logging; }
+            set { Set(() => Logging, ref _logging, value); }
+        }
+
+        public static string DuplicatesDirectory => Path.Combine(Environment
+            .GetFolderPath(Environment.SpecialFolder.MyVideos),
+            "Duplicates");
+    }
+}
