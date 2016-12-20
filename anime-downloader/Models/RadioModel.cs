b479c1eafed8fc598452b33234f157c4d9d1b41a
Duke <dukemiller928@hotmail.com>
2016-12-20 15:24:58 -0700

    Add radio model for use in MVVM collections

---
+++ anime-downloader/Models/RadioModel.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace anime_downloader.Models
+{
+    public class RadioModel : ViewModelBase
+    {
+        #region Header
+        private string _header = string.Empty;
+        public string Header
+        {
+            get { return _header; }
+            set { Set(() => Header, ref _header, value); }
+        }
+        #endregion Header
+
+        #region ToolTip
+        private string _tooltip = string.Empty;
+        public string ToolTip
+        {
+            get { return _tooltip; }
+            set { Set(() => ToolTip, ref _tooltip, value); }
+        }
+        #endregion ToolTip
+
+        public int Purpose { get; set; }
+
+        public string Tag { get; set; }
+
+        public bool Equals(RadioModel obj)
+        {
+            return Header.Equals(obj.Header);
+        }
+    }
+}
