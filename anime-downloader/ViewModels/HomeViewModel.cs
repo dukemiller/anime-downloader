5f1db55b2b2ec8338b86ef760e85370255521452
Duke <dukemiller928@hotmail.com>
2016-12-27 23:38:47 -0700

    Create and display version number
    I'm hoping I can remember to update the version every so often ...

--- anime-downloader/ViewModels/HomeViewModel.cs
+++ anime-downloader/ViewModels/HomeViewModel.cs
@@ -1,7 +1,24 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Reflection;
+using GalaSoft.MvvmLight;
 
 namespace anime_downloader.ViewModels
 {
     public class HomeViewModel : ViewModelBase
-    {}
+    {
+        private string _version;
+
+        public HomeViewModel()
+        {
+            Version = Assembly.GetExecutingAssembly()
+                .GetName()
+                .Version
+                .ToString();
+        }
+        
+        public string Version
+        {
+            get { return _version; }
+            set { Set(() => Version, ref _version, value); }
+        }
+    }
 }
