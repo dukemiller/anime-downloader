6dbe009cf97db1e2e9a974850731da1a8038f4df
Duke <dukemiller928@hotmail.com>
2017-02-12 04:04:58 -0700

    Small displaylog change

--- anime-downloader/ViewModels/Components/DownloadLogViewModel.cs
+++ anime-downloader/ViewModels/Components/DownloadLogViewModel.cs
@@ -25,14 +25,15 @@ namespace anime_downloader.ViewModels.Components
 
         public async void DisplayLogResults()
         {
-            Text = ">> No downloads have been logged so far.\n";
-
+            Text = "";
             if (File.Exists(_settings.PathConfig.Logging))
                 using (var reader = new StreamReader(_settings.PathConfig.Logging))
                 {
                     var data = await reader.ReadToEndAsync();
                     Text = await Task.Run(() => string.Join("\n", data.Split('\n').Reverse().Skip(1)));
                 }
+            else
+                Text = ">> No downloads have been logged so far.";
         }
     }
 }
