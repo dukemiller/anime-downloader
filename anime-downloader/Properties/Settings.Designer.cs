3819172ff1459793b40cc4accd3c3f7d87f91cee
Duke <dukemiller928@hotmail.com>
2017-04-13 19:30:26 -0700

    .NET update 4.5.2 to 4.6

--- anime-downloader/Properties/Settings.Designer.cs
+++ anime-downloader/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace anime_downloader.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -71,9 +71,9 @@ namespace anime_downloader.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        public global::anime_downloader.Models.Configurations.MyAnimeListConfiguration MyAnimeListConfiguration {
+        public global::anime_downloader.Models.Configurations.MyAnimesListConfiguration MyAnimeListConfiguration {
             get {
-                return ((global::anime_downloader.Models.Configurations.MyAnimeListConfiguration)(this["MyAnimeListConfiguration"]));
+                return ((global::anime_downloader.Models.Configurations.MyAnimesListConfiguration)(this["MyAnimeListConfiguration"]));
             }
             set {
                 this["MyAnimeListConfiguration"] = value;
