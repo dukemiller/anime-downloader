04f4cddcdb8df20c01575dcc0c968882ebc851c5
Duke <dukemiller928@hotmail.com>
2017-07-02 23:01:06 -0700

    Add static holder for api keys for anilist

---
+++ anime-downloader/Classes/ApiKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace anime_downloader.Classes
+{
+    /// <summary>
+    ///     All required API keys for services.
+    /// </summary>
+    public static partial class ApiKeys
+    {
+        public static readonly string AniListId = "";
+        public static readonly string AniListSecret = "";
+    }
+}
