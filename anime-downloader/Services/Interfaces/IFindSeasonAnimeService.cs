831d69c277b8cc859aa698ed04fff0838e39bb63
Duke <dukemiller928@hotmail.com>
2017-07-02 23:01:53 -0700

    Add findseasonanime interface

---
+++ anime-downloader/Services/Interfaces/IFindSeasonAnimeService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using anime_downloader.Models;
+using anime_downloader.Models.AniList;
+
+namespace anime_downloader.Services.Interfaces
+{
+    public interface IFindSeasonAnimeService
+    {
+        Task<IEnumerable<AiringAnime>> New(AnimeSeason season);
+        Task<IEnumerable<AiringAnime>> Leftover(AnimeSeason season);
+    }
+}
