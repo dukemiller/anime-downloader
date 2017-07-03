221c51c5d5ee1e47973e5d2604d800926c922b26
Duke <dukemiller928@hotmail.com>
2017-07-02 22:57:49 -0700

    Add more animeseason methods

--- anime-downloader/Models/AnimeSeason.cs
+++ anime-downloader/Models/AnimeSeason.cs
@@ -19,5 +19,41 @@ namespace anime_downloader.Models
 
         [XmlIgnore]
         public string Title => $"{Season.Description()} {Year}";
+
+        public AnimeSeason Next()
+        {
+            var season = Season == Season.Fall ? Season.Winter : Season+1;
+            var year = season == Season.Winter ? Year+1 : Year;
+            return new AnimeSeason
+            {
+                Season = season,
+                Year = year
+            };
+        }
+        
+        public AnimeSeason Previous()
+        {
+            var season = Season - 1 <= 0 ? Season.Fall : Season - 1;
+            var year = Season == Season.Winter ? Year - 1 : Year;
+            return new AnimeSeason {Season = season, Year = year};
+        }
+
+        private AnimeSeason Applier(Func<AnimeSeason, AnimeSeason> func, int amount)
+        {
+            var that = this;
+            for (var i = 0; i < amount; i++)
+                that = func(that);
+            return that;
+        }
+
+        public AnimeSeason Next(int amount) => Applier(animeSeason => animeSeason.Next(), amount);
+
+        public AnimeSeason Previous(int amount) => Applier(animeSeason => animeSeason.Previous(), amount);
+
+        public static AnimeSeason Current => new AnimeSeason
+        {
+            Year = DateTime.Now.Year,
+            Season = (Season) Math.Ceiling(Convert.ToDouble(DateTime.Now.Month) / 3)
+        };
     }
 }
