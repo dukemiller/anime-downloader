475ac0b55f9f0d07a26bddeeef8828f63e23c444
Duke <dukemiller928@hotmail.com>
2017-07-02 23:04:43 -0700

    Add all anilist api models

---
+++ anime-downloader/Models/AniList/Airing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace anime_downloader.Models.AniList
+{
+    public class Airing
+    {
+        [JsonProperty("time")]
+        public DateTime? Time { get; set; }
+
+        [JsonProperty("countdown")]
+        public int? Countdown { get; set; }
+
+        [JsonProperty("next_episode")]
+        public int? NextEpisode { get; set; }
+    }
+}
