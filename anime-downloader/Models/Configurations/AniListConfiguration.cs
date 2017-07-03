ea10a44f0ca9cd98eb6a8bee6cba1e85f6479e99
Duke <dukemiller928@hotmail.com>
2017-07-02 23:03:14 -0700

    Add credential and configuration for anilist

---
+++ anime-downloader/Models/Configurations/AniListConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using anime_downloader.Models.AniList;
+using GalaSoft.MvvmLight;
+
+namespace anime_downloader.Models.Configurations
+{
+    [Serializable]
+    public class AniListConfiguration: ObservableObject
+    {
+        private ClientCredentials _credentials;
+
+        public ClientCredentials Credentials
+        {
+            get => _credentials;
+            set => Set(() => Credentials, ref _credentials, value);
+        }
+    }
+}
