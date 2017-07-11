5c73d81a11b57e0534dfaae264973ada2fbb5263
Duke <dukemiller928@hotmail.com>
2017-07-11 09:12:09 -0700

    Remove focus outline in manage fileviews

--- anime-downloader/Properties/AssemblyInfo.cs
+++ anime-downloader/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.28.13.*")]
+[assembly: AssemblyVersion("0.28.14.*")]
 // [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: NeutralResourcesLanguage("en")]
